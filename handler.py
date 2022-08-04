c36e98ce828a162d50680052a3d6510b3f189b8a
Georgios Roros <georgiosroros@Georgioss-MBP-2.lan>
2022-08-04 17:21:17 -0600

    initial commit

---
+++ handler.py
@@ -0,0 +1,17 @@
+import json
+import boto3
+import os
+
+StateMachine = boto3.client('stepfunctions')
+
+
+def create_new_roofer_webhook(event, context):
+    stateMahcineArn = os.environ.get('STATE_MACHINE_ARN') 
+    response = StateMachine.start_execution(
+        stateMachineArn=stateMahcineArn,
+        name='Create-New-Roofer',
+        input=json.dumps(event)
+    )
+    response = {"statusCode": 200, "body": json.dumps(response)}
+
+    return response
