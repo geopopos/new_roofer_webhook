4f1f578cd92830b0934e2c0dbc08fa4641604690
Georgios Roros <georgiosroros@georgioss-mbp-2.lan>
2022-08-04 18:15:15 -0600

    removed response return

--- handler.py
+++ handler.py
@@ -7,12 +7,11 @@ StateMachine = boto3.client('stepfunctions')
 
 def create_new_roofer_webhook(event, context):
     stateMahcineArn = os.environ.get('STATE_MACHINE_ARN') 
-    print(stateMahcineArn)
     response = StateMachine.start_execution(
         stateMachineArn=stateMahcineArn,
         name='Create-New-Roofer',
         input=json.dumps(event)
     )
-    response = {"statusCode": 200, "body": json.dumps(response)}
+    response = {"statusCode": 200}
 
     return response
