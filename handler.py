911eaf1c9eaef7aa1ddffa3d05f98d7d6d57c3f0
Georgios Roros <georgiosroros@georgioss-mbp-2.lan>
2022-08-04 18:11:11 -0600

    update arn

--- handler.py
+++ handler.py
@@ -7,6 +7,7 @@ StateMachine = boto3.client('stepfunctions')
 
 def create_new_roofer_webhook(event, context):
     stateMahcineArn = os.environ.get('STATE_MACHINE_ARN') 
+    print(stateMahcineArn)
     response = StateMachine.start_execution(
         stateMachineArn=stateMahcineArn,
         name='Create-New-Roofer',
